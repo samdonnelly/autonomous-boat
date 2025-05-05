5bb76613d0bf50384371ef51e1f17c9409d83531
Sam <samueldonnelly11@gmail.com>
2025-05-04 20:25:02 -0600

    Updated the system config to match the autopilot template.

--- hardware/system_config.h
+++ hardware/system_config.h
@@ -35,22 +35,25 @@
 #define VS_SYSTEM_ID 1 
 #define VS_SYSTEM_ID_GCS 255 
 
-// Timing - based on tick period 
-#define VS_HEARTBEAT_TIMEOUT 200   // Max count before heartbeat timeout 
+// Timing 
+#define VS_HEARTBEAT_TIMEOUT 100   // Max count before heartbeat timeout 
 #define VS_MISSION_TIMEOUT 10      // Max count before mission protocol timeout 
 #define VS_MISSION_RESEND 5        // Max tries to resend a mission message 
 #define VS_NAV_DEVICE_TIMEOUT 10   // Max count before navigation devices are considered lost 
 #define VS_RC_TIMEOUT 10           // Max count before RC comms are considered lost 
 
 // Data sizes 
-#define VS_TELEMETRY_BUFF 1000   // Telemetry data buffer size (bytes) 
+#define VS_TELEMETRY_BUFF 1000     // Telemetry data buffer size (bytes) 
+
+// Propulsion and steering 
+#define VS_MOTOR_PWM_OFF 1520      // PWM to turn motor(s) off - can vary between motors/ESCs 
 
 // The vehicle specific settings below only have an affect when the corresponding vehicle 
 // type (VT) is selected. 
 
 // Boat 
-#define VS_BOAT_K1 1   // Kinematics 1 - Differential thruster - 2 propellers 
-#define VS_BOAT_K2 0   // Kinematics 2 - Rudder - 1 propeller + 1 rudder 
+#define VS_BOAT_K1 1               // Kinematics 1 - Differential thruster - 2 propellers 
+#define VS_BOAT_K2 0               // Kinematics 2 - Rudder - 1 propeller + 1 rudder 
 
 //=======================================================================================
 
